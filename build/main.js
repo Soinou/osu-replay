a981ee489f578ab14f5cca6afab47da8fa105463
Soinou <abricot.soinou@gmail.com>
2016-02-10 13:44:46 +0100

    Forgot to add the build folder to .gitignore

--- build/main.js
+++
@@ -1,8 +0,0 @@
-import kernel from "./Kernel";
-// Get the application from the kernel
-var application = kernel.resolve("IApplication");
-// Setup application
-application.setup();
-// Then start it
-application.start();
-//# sourceMappingURL=main.js.map

b87fb30ff3ae86b396d04cbb051a462d34abc154
Soinou <abricot.soinou@gmail.com>
2016-03-28 22:36:05 +0200

    Forgot a few things

--- OsuReplay.Utils/Properties/AssemblyInfo.cs
+++ OsuReplay.Utils/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OsuReplay.Utils")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Useful stuff")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OsuReplay.Utils")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © osu!Replay 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
